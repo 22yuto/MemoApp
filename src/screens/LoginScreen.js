8a7644cc994eb8e794ef3a1fb8d40ed516bfdd78
yuto22 <since2018yuto22@gmail.com>
2020-07-07 18:49:43 +0900

    CreateLogin & Signup

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import {
+  StyleSheet, View, TextInput, TouchableHighlight, Text,
+} from 'react-native'
+
+export default function LoginScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>ログイン</Text>
+      <TextInput style={styles.input} value="Email Adress" />
+      <TextInput style={styles.input} value="PassWord" />
+      <TouchableHighlight style={styles.button} onPress={() => { }} underlayColor="#c70f66">
+        <Text style={styles.buttonTitle}>ログインする</Text>
+      </TouchableHighlight>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    padding: 24,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 28,
+    alignSelf: 'center',
+    marginBottom: 24,
+  },
+  input: {
+    backgroundColor: '#eee',
+    height: 48,
+    marginBottom: 16,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    padding: 8,
+  },
+  button: {
+    backgroundColor: '#e31676',
+    height: 48,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '70%',
+    alignSelf: 'center',
+  },
+  buttonTitle: {
+    color: '#fff',
+    fontSize: 18,
+  },
+})
